2f0f846dafa6bd7a2aed74b997cf2283a781348a
LeonardoMarques7 <leonardoemcs155@gmail.com>
2025-04-04 13:13:02 -0300

    Feat: Alteration case

--- front-end/src/components/SongList.jsx
+++ front-end/src/components/SongList.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay } from "@fortawesome/free-solid-svg-icons";
-import "./Songlist.css";
+import "./SongList.css";
 import { songsArray } from "../assets/database/Songs";
 import { albunsArray } from "../assets/database/Albuns";
 import { Link } from "react-router";
