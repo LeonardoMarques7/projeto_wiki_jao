79a51ff6ed130984995c9aec039860c9f4e83d61
LeonardoMarques7 <leonardoemcs155@gmail.com>
2025-04-04 13:18:03 -0300

    Feat:alteration func async

--- front-end/src/api/api.js
+++ front-end/src/api/api.js
@@ -2,10 +2,23 @@ import axios from "axios";
 
 const URL = "http://localhost:3000/api";
 
-const responseAlbuns = await axios.get(`${URL}/albuns`);
-const responseSongs = await axios.get(`${URL}/songs`);
-const responseShows = await axios.get(`${URL}/shows`);
+export async function loadData() {
+  try {
+    const responseAlbuns = await axios.get(`${URL}/albuns`);
+    const responseSongs = await axios.get(`${URL}/songs`);
+    const responseShows = await axios.get(`${URL}/shows`);
 
-export const albunsArray = responseAlbuns.data;
-export const songsArray = responseSongs.data;
-export const showsArray = responseShows.data;
+    const albunsArray = responseAlbuns.data;
+    const songsArray = responseSongs.data;
+    const showsArray = responseShows.data;
+
+    return { albunsArray, songsArray, showsArray };
+  } catch (error) {
+    console.error("Erro ao buscar dados:", error);
+    return {
+      albunsArray: [],
+      songsArray: [],
+      showsArray: []
+    };
+  }
+}
