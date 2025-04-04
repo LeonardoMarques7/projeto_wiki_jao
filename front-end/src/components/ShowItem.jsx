a34a8b78903b0408954c2b3f0931d7536d76cdea
LeonardoMarques7 <leonardoemcs155@gmail.com>
2025-04-04 12:47:08 -0300

    Feat: Alterations responsive

--- front-end/src/components/ShowItem.jsx
+++ front-end/src/components/ShowItem.jsx
@@ -12,11 +12,13 @@ const ShowItem = ({ data, location, city, state }) => {
 
 	return (
 		<div className="show__card">
-			<h3 className="show__data">{`${formatDate(data)}`}</h3>
-			<div className="show__location">
-				<h4 className="font-bold show__title">{location}</h4>
-				<h4 className="show__city">{`${city} / ${state}`}</h4>
-			</div>
+			<span className="header__card">
+				<h3 className="show__data">{`${formatDate(data)}`}</h3>
+				<div className="show__location">
+					<h4 className="font-bold show__title">{location}</h4>
+					<h4 className="show__city">{`${city} / ${state}`}</h4>
+				</div>
+			</span>
 			<div className="show__action">
 				<button className="show__button--info">Mais informações</button>
 				<button className="show__button--buy">Comprar Ingressos</button>
